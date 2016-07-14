f0bd42a0f3c8901b58c7fe50873a4fc16260a397
cruz02148 <cruz02148@gmail.com>
2016-07-14 00:54:56 -0400

    Rough draft of LTI new booking

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+injectTapEventPlugin();
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
