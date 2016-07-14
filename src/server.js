c08e57d486afeb31b62c2a81a48a3547f34da655
cruz02148 <cruz02148@gmail.com>
2016-07-14 01:54:33 -0400

    test

--- src/server.js
+++ src/server.js
@@ -1,8 +1,29 @@
-const express = require('express');
-const path = require('path');
+const path = require('path')
+const express = require('express')
+
+const Server = require('./server.js')
+const port = (process.env.PORT || 8080)
 const app = express();
-const port = process.env.PORT || 8080;
 
-app.use(express.static(`${__dirname}/index.html`));
+const indexPath = path.join(__dirname, '/../index.html')
+const publicPath = express.static(path.join(__dirname, '../public'))
 
-app.listen(port);
+app.use('/public', publicPath)
+app.get('/', function (_, res) { res.sendFile(indexPath) })
+
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
+  const config = require('../webpack.dev.config.js')
+  const compiler = webpack(config)
+
+  app.use(webpackHotMiddleware(compiler))
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }))
+}
+
+app.listen(port)
+console.log(`Listening at http://localhost:${port}`)
